ed285aa6f0ab737fa2792247637c94a7bfde218c
RCmags <memoryofatrufestival@gmail.com>
2024-06-18 16:55:08 -0400

    update files - partial function

---
+++ References.md
@@ -0,0 +1,40 @@
+# Mongoose 
+
+__CRUD functions__:
+
+https://stackoverflow.com/questions/52141790/findbyidandremove-is-not-a-function
+
+https://www.geeksforgeeks.org/mongoose-findbyidandremove-function/
+
+https://www.slingacademy.com/article/mongoose-findbyidandupdate-function-tutorial-examples/
+
+https://www.geeksforgeeks.org/mongoose-deleteone-function/?ref=ml_lbp
+
+https://stackoverflow.com/questions/37347802/find-by-id-with-mongoose
+
+https://www.slingacademy.com/article/mongoose-how-to-remove-all-documents-from-a-collection/
+
+__Class inheritance__:
+
+https://techinsights.manisuec.com/mongodb/mongoose-discriminator-non-dry-way-inherit-properties/
+
+https://stackoverflow.com/questions/18317284/mongoose-extending-schemas
+
+__Entry id__:
+
+https://dev.devbf.com/posts/how-can-i-convert-a-string-to-an-objectid-in-nodejs-with-mongoose-d3f0c/
+
+
+
+# Javascript
+
+__Misc__:
+https://stackoverflow.com/questions/21978392/modify-a-variable-inside-a-function
+
+https://www.freecodecamp.org/news/javascript-check-if-undefined-how-to-test-for-undefined-in-js/
+
+__Dates__:
+https://www.w3schools.com/howto/howto_js_get_date.asp
+
+https://www.freecodecamp.org/news/javascript-date-format-how-to-format-a-date-in-js/
+
--- index.js
+++ index.js
@@ -3,16 +3,169 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+// read forms
+const bodyParser = require('body-parser'); 
+app.use( bodyParser.urlencoded({extended:true}) ); 
+
+// mongodb - store users
+mongoose = require('mongoose');
+mongoose.connect( process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//---------------- boilerplate ----------------
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+//---------------- custom code ---------------
+
+/* --- database functions --- */
+
+// user schema
+const userSchema = new mongoose.Schema({
+  username: {type: String, required: true},
+  log: [{
+    description: {type: String, required: true},
+    duration: {type: Number, required: true},
+    date: Date
+  }],
+  count: Number
+});
+
+let UserLog =  mongoose.model('UserLog', userSchema);
+
+// wipe data
+UserLog.deleteMany({})
+.then(x => console.log("wiped data"))
+.catch(error => console.log(error));
+
+//example person
+/*
+const personData = new UserLog(
+  { name: "name", 
+    log: [
+      {description: "desc", duration: 3, date: "1993-03-13"}
+    ]
+  }
+);
+console.log(personData);
+*/
+
+// save user
+function saveUser(user) {
+  user.save()
+  .then( data => console.log("saved user") )
+  .catch( error => console.log(error) );
+}
+
+// get user
+function getUser(id, fdone) {
+  UserLog.findById(id)
+  .then( data => {
+    console.log("retrieved user"); 
+    fdone(data); // do something with data
+    console.log(data);
+  })
+  .catch( error => console.log(error) );
+}
+
+// update user
+function updateUser(id, data_new) {
+  UserLog.findByIdAndUpdate(id, data_new)
+  .then( data => console.log("updated user") )
+  .catch( error => console.log(error) );
+}
+
+// delete user
+function removeUser(id) {
+  UserLog.findByIdAndDelete(id)
+  .then( data => console.log("removed user") )
+  .catch( error => console.log(error) );
+}
+
+// test
+/*
+saveUser(personData);
+getUser(personData._id, x => { console.log(x) } );
+updateUser(personData._id, { name: "bob" } );
+getUser(personData._id, x => { console.log(x) } );
+removeUser(personData._id);
+*/
+// retrieve user
+
+/* --- user functions --- */
+
+// read create user
+app.post("/api/users",  
+  function (req, res) {
+    // form input
+    var username = req.body.username;
+    console.log("submitted user");
+
+    // new entry
+    const newUser = new UserLog( { username: username } );
+    saveUser(newUser);
+    // output
+    res.json({
+      username: username,
+      _id: newUser._id
+    });
+  }
+);
+
+// get all users
+app.get("/api/users",  
+  function (req, res) {
+    UserLog.find({})
+    .sort( {username: 1} )
+    .select( {username: 1, _id: 1} )
+    .then( data => {
+      console.log(data);
+      res.json( data ); 
+    })
+    .catch( error => console.log(error) );
+  }
+);
+
+/* --- exercise functions --- */
+
+app.post("/api/users/:_id/exercises",  
+  function (req, res) {
+    // form input
+    var id = req.body._id;
+    var description = req.body.description;
+    var duration = req.body.duration;
+    var date = req.body.date;
+
+    //filter
+    if( date.length === 0 ) {
+      var date_now = new Date();
+      date = date_now.getFullYear() + "-" + 
+             date_now.getMonth() + "-" + 
+             date_now.getDate();
+      console.log(date);
+    } 
+
+    // output
+    /*
+    res.json({
+      _id: id,
+      description: description,
+      duration: duration,
+      date: date
+    });*/
+    function fdone(data) {
+      console.log("access data");
+      res.json(data);
+    }
+    var obj_id = new mongoose.Types.ObjectId(id);
+    console.log(obj_id);
+    getUser( obj_id, fdone); 
+  }
+);
